eba4e2a4721342d37cc647884cc1099f2b4da582
RAGAV-24 <ragavrunner33@gmail.com>
2024-09-16 18:16:24 +0530

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,25 @@
+import { CONTACT } from "../constants"
+import { motion } from "framer-motion"
+const Contact = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-20">
+        <motion.h1 
+        whileInView={{y:0,opacity:1}}
+        initial={{y:-100,opacity:0}}
+       transition={{duration:1}}
+        className="my-10 text-center text-4xl">Get in Touch</motion.h1>
+        <motion.div 
+        whileInView={{y:0,opacity:1}}
+        initial={{y:-100,opacity:0}}
+       transition={{duration:2}}
+        className="text-center tracking-tighter">
+            <p className="my-4">Adress : {CONTACT.address}</p>
+            <p className="my-4">Phone Number : {CONTACT.phoneNo}</p>
+            <p className="my-4">Email : {CONTACT.email}</p>
+        </motion.div>
+      
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/constants/index.js
@@ -0,0 +1,53 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT ='Passionate and detail-oriented Fullstack Developer with hands-on experience in front-end and back-end technologies. Seeking to leverage my skills in JavaScript, React, Node.js, and database management to build scalable web applications. Enthusiastic about problem-solving and delivering efficient solutions. Eager to contribute to innovative projects and grow within a dynamic development team.'
+export const ABOUT_TEXT = 'I’m Ragav R, a passionate Fullstack Developer from Tiruppur, Tamil Nadu, with a strong foundation in JavaScript, Node.js, React, and MongoDB. I thrive on building dynamic, responsive websites and applications that solve real-world problems. Currently pursuing my B.Tech in Artificial Intelligence and Data Science at Kongu Engineering College, I have hands-on experience through internships and personal projects, including an AI-powered resume builder and a modern nursery website. My continuous goal is to leverage my technical skills while constantly learning new technologies. I am excited about contributing to impactful projects and collaborating within innovative teams.'
+export const EXPERIENCES = [
+  {
+    year: "Jul 2024 - Present",
+    role: "Full Stack Developer Intern",
+    company: "Google Inc.",
+    description: `Developed responsive websites using React.js and integrated Groqs APIs to efficiently display and manage data.`,
+    technologies: ["Javascript", "React.js", "mongoDB"],
+  },
+  {
+    year: "Apr 2024 - May 2024",
+    role: "Machine Learning Engineer",
+    company: "InternPe",
+    description: `Gained hands-on experience in building predictive models and performing data analysis using machine learning techniques.`,
+    technologies: ["Decision Tree", "Navie Bayes"],
+  }
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Resume Content Parser",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS","MongoDB"],
+  },
+];
+
+export const CONTACT = {
+  address: "8/31 Stanes Road  Tiruppur  Tamil Nadu ",
+  phoneNo: "+91 63815 60174 ",
+  email: "ragavrunner33@gmail.com",
+};
