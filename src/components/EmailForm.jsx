550f81847fa1d40fa3a21a2eeef046b55763592e
RAGAV-24 <ragavrunner33@gmail.com>
2025-03-13 19:11:35 +0530

    hi

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -27,7 +27,6 @@ const EmailForm = () => {
     hover: { scale: 1.02, borderColor: '#374151', transition: { duration: 0.3 } }, // Gray-700
     initial: { scale: 1, borderColor: '#d1d5db' } // Gray-300
   };
-
   return (
     <div className="border-b border-neutral-900 pb-20">
       <motion.h1
@@ -39,55 +38,57 @@ const EmailForm = () => {
         Get in Touch
       </motion.h1>
 
-      <form className="space-y-6" ref={form} onSubmit={sendEmail}>
-        <motion.input
-          type="text"
-          className="w-full p-3 border border-gray-300 bg-neutral-100 text-neutral-900 rounded-lg transition-transform duration-300 ease-in-out hover:scale-105"
-          placeholder="Your Name"
-          name="your_name"
-          aria-label="Your Name"
-          required
-          variants={inputVariants}
-          initial="initial"
-          whileFocus="focus"
-          whileHover="hover"
-        />
-        <motion.input
-          type="email"
-          className="w-full p-3 border border-gray-300 bg-neutral-100 text-neutral-900 rounded-lg transition-transform duration-300 ease-in-out hover:scale-105"
-          placeholder="Your Email"
-          name="your_email"
-          aria-label="Your Email"
-          required
-          variants={inputVariants}
-          initial="initial"
-          whileFocus="focus"
-          whileHover="hover"
-        />
-        <motion.textarea
-          className="w-full p-3 border border-gray-300 bg-neutral-100 text-neutral-900 rounded-lg transition-transform duration-300 ease-in-out hover:scale-105"
-          name="message"
-          rows="5"
-          placeholder="Your Message"
-          aria-label="Your Message"
-          required
-          variants={inputVariants}
-          initial="initial"
-          whileFocus="focus"
-          whileHover="hover"
-        ></motion.textarea>
-        <motion.button
-          type="submit"
-          value="Send"
-          className="w-full p-3 bg-gray-600 text-white rounded-lg hover:bg-gray-700 transition-transform duration-300 ease-in-out"
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          Submit
-        </motion.button>
-      </form>
+      <div className="  max-w-md mx-auto">
+        <form className="space-y-6 " ref={form} onSubmit={sendEmail}>
+          <motion.input
+            type="text"
+            className="w-full p-3 border border-gray-300 bg-neutral-100 text-neutral-900 rounded-lg transition-transform duration-300 ease-in-out hover:scale-105"
+            placeholder="Your Name"
+            name="your_name"
+            aria-label="Your Name"
+            required
+            variants={inputVariants}
+            initial="initial"
+            whileFocus="focus"
+            whileHover="hover"
+          />
+          <motion.input
+            type="email"
+            className="w-full p-3 border border-gray-300 bg-neutral-100 text-neutral-900 rounded-lg transition-transform duration-300 ease-in-out hover:scale-105"
+            placeholder="Your Email"
+            name="your_email"
+            aria-label="Your Email"
+            required
+            variants={inputVariants}
+            initial="initial"
+            whileFocus="focus"
+            whileHover="hover"
+          />
+          <motion.textarea
+            className="w-full p-3 border border-gray-300 bg-neutral-100 text-neutral-900 rounded-lg transition-transform duration-300 ease-in-out hover:scale-105"
+            name="message"
+            rows="5"
+            placeholder="Your Message"
+            aria-label="Your Message"
+            required
+            variants={inputVariants}
+            initial="initial"
+            whileFocus="focus"
+            whileHover="hover"
+          ></motion.textarea>
+          <motion.button
+            type="submit"
+            value="Send"
+            className="w-full p-3 bg-gray-600 text-white rounded-lg hover:bg-gray-700 transition-transform duration-300 ease-in-out"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            Submit
+          </motion.button>
+        </form>
+      </div>
     </div>
   );
-};
+  };
 
 export default EmailForm;
--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,7 @@ export const HERO_CONTENT ='Passionate and detail-oriented Fullstack Developer w
 export const ABOUT_TEXT = 'I’m Ragav R, a passionate Fullstack Developer from Tiruppur, Tamil Nadu, with a strong foundation in JavaScript, Node.js, React, and MongoDB. I thrive on building dynamic, responsive websites and applications that solve real-world problems. Currently pursuing my B.Tech in Artificial Intelligence and Data Science at Kongu Engineering College, I have hands-on experience through internships and personal projects, including an AI-powered resume builder and a modern nursery website. My continuous goal is to leverage my technical skills while constantly learning new technologies. I am excited about contributing to impactful projects and collaborating within innovative teams.'
 export const EXPERIENCES = [
   {
-    year: "Jul 2024 - Present",
+    year: "Jul 2024 - Nov-2024",
     role: "Full Stack Developer Intern",
     company: "Shine Logics.",
     description: `Developed responsive websites using React.js and integrated Groqs APIs to efficiently display and manage data.`,
@@ -29,7 +29,7 @@ export const PROJECTS = [
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
     technologies: [ "React", "Node.js", "MongoDB"],
   },
-  
+
   {
     title: "OD Claimming Website",
     image: project3,
@@ -38,10 +38,10 @@ export const PROJECTS = [
     technologies: [ "React Vite", "TailWind CSS","MongoDB","Node.js"],
   },
   {
-    title: "Resume Content Parser",
+    title: "Medical ChatBot",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+      "A medical Menal health chatbot which can change your mood of mind while talking",
     technologies: ["React","MongoDB","Node.js"],
   },
 ];
