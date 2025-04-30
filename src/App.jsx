24f4dd62a95729aac5698952f9f5034915568088
RAGAV-24 <ragavrunner33@gmail.com>
2025-04-30 12:03:37 +0530

    changed background

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,17 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Email from "./components/EmailForm";
-import ParticleCursor from "./components/ParticleCursor"; // Import new component
+// import ParticleCursor from "./components/ParticleCursor";
+import VantaBackground from "./components/VantaBackground"; // 👈 New import
+import SplashCursor from './SplashCursor'
+
 
 const App = () => {
   return (
     <>
-      <ParticleCursor /> {/* Add the particle cursor component */}
+    {/* <SplashCursor /> */}
+      <VantaBackground /> {/* 👈 Add this behind all content */}
       <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-        <div className="fixed top-0 -z-10 h-full w-full">
-          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-        </div>
         <div className="container relative z-20 mx-auto px-8">
           <Navbar />
           <Hero />
